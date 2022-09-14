9458cad7e375f7b023e6809074c0d21e87c09614
bouzoubaafahd <fahde.bouzoubaa@etu.univ-cotedazur.fr>
2022-09-14 13:42:15 +0200

    db config

--- src/main/java/com/example/ARCHI_CLOUDAPP/ArchiCloudAppApplication.java
+++ src/main/java/com/example/ARCHI_CLOUDAPP/ArchiCloudAppApplication.java
@@ -8,6 +8,12 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ArchiCloudAppApplication implements ApplicationRunner {
 
@@ -23,8 +29,20 @@ public class ArchiCloudAppApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		  getConnection();
           repo.deleteAll();
 		  repo.save(new Count(1L , "Fahde" , "Bouzoubaa" , 0));
 
+
+	}
+
+	private  Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+		return DriverManager.getConnection(dbUrl, username, password);
 	}
 }
